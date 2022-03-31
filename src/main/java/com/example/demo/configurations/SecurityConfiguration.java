44d7e7f112a3f7df83448afd6c5c12c0f931c8dd
vishwejeet04 <vishwejeet.singh@astrotalk.com>
2022-03-31 14:51:53 +0530

    new updates

--- src/main/java/com/example/demo/configurations/SecurityConfiguration.java
+++ src/main/java/com/example/demo/configurations/SecurityConfiguration.java
@@ -1,5 +1,7 @@
 package com.example.demo.configurations;
 
+import com.example.demo.configurations.filters.SecurityFilter;
+import com.example.demo.services.EmployeeDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,48 +15,41 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import com.example.demo.configurations.filters.SecurityFilter;
-import com.example.demo.services.EmployeeDetailService;
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-	@Autowired
-	EmployeeDetailService myuserdetailservice;
+    @Autowired
+    EmployeeDetailService myuserdetailservice;
 
-	@Autowired
-	SecurityFilter securityFilter;
+    @Autowired
+    SecurityFilter securityFilter;
 
-	@Override
-	@Bean
-	public AuthenticationManager authenticationManagerBean() throws Exception {
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
 
-		return super.authenticationManagerBean();
-	}
+        return super.authenticationManagerBean();
+    }
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-		auth.userDetailsService(myuserdetailservice).passwordEncoder(passwordEncoder());
-	}
+        auth.userDetailsService(myuserdetailservice).passwordEncoder(passwordEncoder());
+    }
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().cors().and().authorizeRequests()
-				.antMatchers("/home/**", "/h2-console", "/h2-console/**", "/authenticate", "/register", "/",
-						"/static/**", "/manifest.json", "/favicon.ico", "/logo192.png", "/swagger-ui.html/**", "/v3/**",
-						"/swagger-ui/**", "/swagger/**", "/get/**")
-				.permitAll().anyRequest().authenticated().and().sessionManagement()//
-				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-		http.addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
-		http.headers().frameOptions().sameOrigin();
-	}
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().cors().and().authorizeRequests().antMatchers("/home/**", "/h2-console", "/h2-console/**", "/authenticate", "/register", "/", "/static/**", "/manifest.json", "/favicon.ico", "/logo192.png", "/swagger-ui.html/**", "/v3/**", "/swagger-ui/**", "/swagger/**", "/get/**").permitAll().anyRequest().authenticated().and().sessionManagement()//
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
+        http.headers().frameOptions().sameOrigin();
+    }
 
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return NoOpPasswordEncoder.getInstance();
-	}
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
 
 //	@Bean
 //	public CorsFilter corsFilter() {
